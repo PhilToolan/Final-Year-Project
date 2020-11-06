812334c3bb10470e59d28b369ed1e5996cf3da05
PhilToolan <47186301+PhilToolan@users.noreply.github.com>
2020-11-06 23:01:15 +0000

    Initial commit

---
+++ GameServer.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace GameServer
+{
+    
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Big Phil");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ GameServer/GameServer/ServerHandle.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GameServer
+{
+    class ServerHandle
+    {
+        public static void WelcomeReceived(int _fromClient, Packet _packet)
+        {
+            int _clientCheck = _packet.ReadInt();
+            string _username = _packet.ReadString();
+
+            Console.WriteLine($"{Server.clients[_fromClient].tcp.socket.Client.RemoteEndPoint} connected successfully and is now player {_fromClient}");
+            //Error Checking
+/*            if(_fromClient != _clientIdCheck)
+            {
+                Console.WriteLine($"Player \"{_username}\" (ID: {_fromClient}) has assumed the wrong client ID ({_clientIdCheck})!");
+            }*/
+            // TODO: Send player into game
+        }
+
+        public static void UDPTestReceived(int _fromClient, Packet _packet)
+        {
+            string _msg = _packet.ReadString();
+
+            Console.WriteLine($"Received packet via UDP. Message contents: {_msg}");
+        }
+    }
+}
