087c4a7b54d51bb1263df9c43ecc71166ba61f24
PhilToolan <47186301+PhilToolan@users.noreply.github.com>
2021-03-03 09:39:00 +0000

    Controller Player working

---
+++ VR/Assets/Scripts/ButtonSelect.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonSelect : MonoBehaviour
+{
+
+    public GameObject door;
+
+    void OnMouseDown()
+    {
+        door.SetActive(true);
+    }
+
+}
