6350ae73b9a241ab1dceee28f0934134b8c4cdf7
PhilToolan <47186301+PhilToolan@users.noreply.github.com>
2021-03-15 14:52:04 +0000

    Implementing Controller player UI

---
+++ VR/Assets/Scripts/ScreenButton.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenButton : MonoBehaviour
+{
+
+    public GameObject screenmenu;
+
+    void OnMouseDown()
+    {
+        screenmenu.SetActive(true);
+    }
+
+}
