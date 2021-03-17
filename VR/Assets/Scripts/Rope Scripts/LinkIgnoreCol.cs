21bb94c635d4e8138338f47dd90e064b71bd0ff1
PhilToolan <47186301+PhilToolan@users.noreply.github.com>
2021-03-17 14:13:14 +0000

    Rope Physics implemented for wires

---
+++ VR/Assets/Scripts/Rope Scripts/LinkIgnoreCol.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LinkIgnoreCol : MonoBehaviour
+{
+
+    void Start()
+    {
+        Physics.IgnoreLayerCollision(9, 11);
+    }
+
+}
---
+++ VR/Assets/Scripts/Rope Scripts/Rope.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rope : MonoBehaviour
+{
+
+	public Rigidbody hook;
+
+	public GameObject linkPrefab;
+
+	//public Weight weigth;
+
+	public int links = 7;
+
+	void Start()
+	{
+		GenerateRope();
+	}
+
+	void GenerateRope()
+	{
+		Rigidbody previousRB = hook;
+		for (int i = 0; i < links; i++)
+		{
+			GameObject link = Instantiate(linkPrefab, transform);
+			HingeJoint joint = link.GetComponent<HingeJoint>();
+			joint.connectedBody = previousRB;
+
+			previousRB = link.GetComponent<Rigidbody>();
+
+			/*			if (i < links - 1)
+						{
+							previousRB = link.GetComponent<Rigidbody>();
+						}
+						else
+						{
+							weigth.ConnectRopeEnd(link.GetComponent<Rigidbody>());
+						}*/
+
+
+		}
+	}
+
+}
