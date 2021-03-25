1ebfaf0ed06d51f1cb9484553a361a9652953f6b
PhilToolan <47186301+PhilToolan@users.noreply.github.com>
2021-03-25 18:14:39 +0000

    Buttons

---
+++ VR/Assets/Scripts/MenuHandler.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuHandler : MonoBehaviour
+{
+
+    [SerializeField] private string tutorial;
+    [SerializeField] private string mission1;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void LoadTutorial()
+    {
+        SceneManager.LoadScene(tutorial);
+    }
+
+    public void LoadMission()
+    {
+        SceneManager.LoadScene(mission1);
+    }
+
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
