80f5bd374c9bd679e556557089396d65e3a4757b
PhilToolan <47186301+PhilToolan@users.noreply.github.com>
2021-04-05 12:06:31 +0100

    New messages

---
+++ VR/Assets/Scripts/WallInteractionButton.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WallInteractionButton : MonoBehaviour
+{
+
+    public GameObject wallmenu;
+    public Text wallText;
+    [SerializeField] private string message;
+    public int accessPoints = 1;
+
+    void OnMouseDown()
+    {
+        DisplayMessage();
+        wallmenu.SetActive(true);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            wallmenu.SetActive(false);
+        }
+
+        if (accessPoints <= 1)
+        {
+            message = "Need more access points!";
+        }
+        if (accessPoints > 1)
+        {
+            message = "I love YOU ";
+        }
+    }
+
+    void DisplayMessage()
+    {
+        wallText.text = message;
+    }
+
+    public void IncreasePoints()
+    {
+        accessPoints += 1;
+    }
+
+}
